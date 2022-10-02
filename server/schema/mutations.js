f824bb3ee7a8861a31a2844223c3a74a3e1eccc7
maevanapcontact <maevanap.contact@gmail.com>
2022-10-02 14:06:31 +0200

    feat(mutation): add Quest mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -4,10 +4,12 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLID,
+  GraphQLEnumType,
 } = require("graphql");
 
 const Character = require("../models/Character");
-const { CharacterType } = require("./types/types");
+const Quest = require("../models/Quest");
+const { CharacterType, QuestType } = require("./types/types");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -16,7 +18,17 @@ const mutation = new GraphQLObjectType({
       type: CharacterType,
       args: {
         name: { type: GraphQLNonNull(GraphQLString) },
-        role: { type: GraphQLNonNull(GraphQLString) },
+        role: {
+          type: new GraphQLEnumType({
+            name: "CharacterRole",
+            values: {
+              tank: { value: "Tank" },
+              heal: { value: "Heal" },
+              dps: { value: "DPS" },
+            },
+          }),
+          defaultValue: "DPS",
+        },
         level: { type: GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
@@ -35,6 +47,94 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parent, args) {
         return Character.findByIdAndRemove(args.id);
+      },
+    },
+    updateCharacter: {
+      type: CharacterType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        role: { type: GraphQLString },
+        level: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return Character.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              role: args.role,
+              level: args.level,
+            },
+          },
+          { new: false }
+        );
+      },
+    },
+    addQuest: {
+      type: QuestType,
+      args: {
+        characterId: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "QuestStatus",
+            values: {
+              todo: { value: "To do" },
+              doing: { value: "In progress" },
+              done: { value: "Done" },
+            },
+          }),
+          defaultValue: "To do",
+        },
+      },
+      resolve(parent, args) {
+        const quest = new Quest({
+          characterId: args.characterId,
+          name: args.name,
+          description: args.description,
+          status: args.status,
+        });
+        return quest.save();
+      },
+    },
+    deleteQuest: {
+      type: QuestType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Quest.findByIdAndRemove(args.id);
+      },
+    },
+    updateQuest: {
+      type: QuestType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: "QuestStatusUpdate",
+            values: {
+              todo: { value: "To do" },
+              doing: { value: "In progress" },
+              done: { value: "Done" },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        return Quest.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true }
+        );
       },
     },
   },
